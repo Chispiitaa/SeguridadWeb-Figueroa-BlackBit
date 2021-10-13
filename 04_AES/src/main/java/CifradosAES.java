2b05bc69e1ad8fe8fd459b070a71c81d40092b0f
Josue-Figueroa <72054559+Josue-Figueroa@users.noreply.github.com>
2021-10-13 13:00:31 -0500

    aes

---
+++ 04_AES/src/main/java/CifradosAES.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.util.*;
+
+public class CifradosAES {
+    
+    public static void main(String[] args) throws Exception{
+        String mensaje = "Habia una vez un patito que decia miau miau y queria mimir todo el dia, y ademas jugaba halo y mataba gente de otros continentes llamados niños rata :3";
+        String mensajeCifrado = AES.encrypt(mensaje);
+        String mensajeDescifrado = AES.decrypt(mensajeCifrado);
+        
+        System.out.println("El mensaje ultrasecreto es: " + mensaje);
+        System.out.println("Mensaje Cifrado AES 128: " + mensajeCifrado);
+        System.out.println("Mensaje Descifrado AES 128: " + mensajeDescifrado);
+    }
+    
+}
